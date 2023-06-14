b4361fdf1ed31d3466c363f8f0f8b2ff1644a34f
sergrigordeev <inghamway@gmail.com>
2023-06-14 17:08:34 +0200

    #42 - SchedulerConfig

---
+++ drones-service/container/src/main/java/com/musala/sg/drones/container/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.musala.sg.drones.container.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
